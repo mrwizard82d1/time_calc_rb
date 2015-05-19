7078d04b2bd70d0156f8dbb19d8daabcad2b46cb
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-05-18 21:39:30 -0400

    Change test framework to shoulda.
    Change test framework to shoulda. Add tests.

--- tests/activity_test.rb
+++ tests/activity_test.rb
@@ -1,16 +1,75 @@
 require 'test/unit'
+require 'shoulda'
 
 require 'time_calc/activity'
 
 
 class ActivityTest < Test::Unit::TestCase
-
   # Test ideas
   # - start 1724
   # - start 0322
   # - start 0004
   # - start 05260655
   # - start 202506220536
+
+context 'full hours and minutes' do
+    setup do
+      @cut = TimeCalc::Activity.new('1724', nil)
+    end
+    should 'have the correct start hour' do
+      assert_equal(17, @cut.start.hour)
+    end
+    should 'have correct start minute' do
+      assert_equal(24, @cut.start.min)
+    end
+  end
+
+  context 'zero hour and full minutes' do
+    setup do
+      @cut = TimeCalc::Activity.new('0322', nil)
+    end
+    should 'have the correct start hour' do
+      assert_equal(3, @cut.start.hour)
+    end
+    should 'have correct start minute' do
+      assert_equal(22, @cut.start.min)
+    end
+  end
+
+  context 'zero hour and zero minute' do
+    setup do
+      @cut = TimeCalc::Activity.new('0004', nil)
+    end
+    should 'have the correct start hour' do
+      assert_equal(0, @cut.start.hour)
+    end
+    should 'have correct start minute' do
+      assert_equal(4, @cut.start.min)
+    end
+  end
+
+  context 'zero month and full day' do
+    setup do
+      @cut = TimeCalc::Activity.new('05260655', nil)
+    end
+    should 'have the correct start month' do
+      assert_equal(5, @cut.start.month)
+    end
+    should 'have correct start day' do
+      assert_equal(26, @cut.start.day)
+    end
+    should 'have the correct start hour' do
+      assert_equal(6, @cut.start.hour)
+    end
+    should 'have correct start minute' do
+      assert_equal(55, @cut.start.min)
+    end
+  end
+end
+
+
+=begin
+class ActivityTest < Test::Unit::TestCase
 
   def setup
     @zero_month_full_day = '05260655'
@@ -62,3 +121,4 @@ class ActivityTest < Test::Unit::TestCase
     assert_equal(26, cut.start.day)
   end
 end
+=end
