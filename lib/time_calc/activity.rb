aa0878dfd2f00f5bbcd56e29b7e859477ce84fbd
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-05-18 20:59:18 -0400

    Move class under test into appropriate directory.
    Move the Activity class into the appropriate path (lib/time_calc). Include code in module (TimeCalc).

---
+++ lib/time_calc/activity.rb
@@ -0,0 +1,27 @@
+module TimeCalc
+  class Activity
+    attr_reader :start
+
+    def initialize(start_text, project)
+      if start_text.size == 4
+        today = Time.new
+        start_year, start_month, start_day = today.year, today.month, today.day
+        start_hour = start_text.slice(0, 2).to_i
+        start_min = start_text.slice(2, 2).to_i
+        start_sec = 0
+        @start = Time.new(start_year, start_month, start_day,
+                          start_hour, start_min, start_sec)
+      elsif start_text.size == 8
+        today = Time.new
+        start_year = today.year
+        start_month = start_text.slice(0, 2).to_i
+        start_day = start_text.slice(2, 2).to_i
+        start_hour = start_text.slice(4, 2).to_i
+        start_min = start_text.slice(6, 2).to_i
+        start_sec = 0
+        @start = Time.new(start_year, start_month, start_day,
+                          start_hour, start_min, start_sec)
+      end
+    end
+  end
+end
--- tests/activity_test.rb
+++ tests/activity_test.rb
@@ -1,30 +1,6 @@
 require 'test/unit'
 
-class Activity
-  attr_reader :start
-  
-  def initialize(start_text, project)
-    if start_text.size == 4
-      today = Time.new
-      start_year, start_month, start_day = today.year, today.month, today.day
-      start_hour = start_text.slice(0, 2).to_i
-      start_min = start_text.slice(2, 2).to_i
-      start_sec = 0
-      @start = Time.new(start_year, start_month, start_day,
-                        start_hour, start_min, start_sec)
-    elsif start_text.size == 8
-      today = Time.new
-      start_year = today.year
-      start_month = start_text.slice(0, 2).to_i
-      start_day = start_text.slice(2, 2).to_i
-      start_hour = start_text.slice(4, 2).to_i
-      start_min = start_text.slice(6, 2).to_i
-      start_sec = 0
-      @start = Time.new(start_year, start_month, start_day,
-                        start_hour, start_min, start_sec)
-      end
-  end
-end
+require 'time_calc/activity'
 
 
 class ActivityTest < Test::Unit::TestCase
@@ -45,43 +21,43 @@ class ActivityTest < Test::Unit::TestCase
 
 
   def test_new_full_hour_full_min_start_hour_set
-    cut = Activity.new(@full_hour_full_min, nil)
+    cut = TimeCalc::Activity.new(@full_hour_full_min, nil)
 
     assert_equal(17, cut.start.hour)
   end
 
   def test_new_full_hour_full_min_start_min_set
-    cut = Activity.new(@full_hour_full_min, nil)
+    cut = TimeCalc::Activity.new(@full_hour_full_min, nil)
 
     assert_equal(24, cut.start.min)
   end
 
   def test_new_zero_hour_full_min_start_hour_set
-    cut = Activity.new(@zero_hour_full_min, nil)
+    cut = TimeCalc::Activity.new(@zero_hour_full_min, nil)
 
     assert_equal(3, cut.start.hour)
   end
 
   def test_new_zero_hour_zero_min_start_hour_set
-    cut = Activity.new(@zero_hour_zero_min, nil)
+    cut = TimeCalc::Activity.new(@zero_hour_zero_min, nil)
 
     assert_equal(0, cut.start.hour)
   end
 
   def test_new_zero_hour_zero_min_start_min_set
-    cut = Activity.new(@zero_hour_zero_min, nil)
+    cut = TimeCalc::Activity.new(@zero_hour_zero_min, nil)
 
     assert_equal(4, cut.start.min)
   end
 
   def test_new_zero_month_full_day_start_month_set
-    cut = Activity.new(@zero_month_full_day, nil)
+    cut = TimeCalc::Activity.new(@zero_month_full_day, nil)
 
     assert_equal(5, cut.start.month)
   end
 
   def test_new_zero_month_full_day_start_day_set
-    cut = Activity.new(@zero_month_full_day, nil)
+    cut = TimeCalc::Activity.new(@zero_month_full_day, nil)
 
     assert_equal(26, cut.start.day)
   end
