ddfeced1f6b4e9d69a7b101e092a8759018870d8
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-05-23 16:54:03 -0400

    Inlined explanatory variables.

--- lib/time_calc/activity.rb
+++ lib/time_calc/activity.rb
@@ -4,17 +4,18 @@ module TimeCalc
 
     def initialize(start_text, _project)
       today = Time.new
-      time_stamp_pattern = /(\d{4}(?=\d{2}\d{2}\d{2}\d{2}))?(\d{2})?(\d{2})?(\d{2})(\d{2})/
+      # "Parse" the starting time text using a regular expression:
+      # Optional 4 digit year (but only if I have 8 digits afterward)
+      # Optional 2 digit month (1 -12 but not checked on input)
+      # Optional 2 digit day of month (1-31 but not checked on input)
+      # Required 2 digit hour
+      # Required 2 digit minute
+      # No seconds (not checked)
+      time_stamp_pattern = /(\d{4}(?=\d{8}))?(\d{2})?(\d{2})?(\d{2})(\d{2})/
       match = time_stamp_pattern.match(start_text)
       raise ArgumentError, "Invalid starting time format: #{start_text}." unless match.size > 0
-      start_year = match[1] || today.year
-      start_month = match[2] || today.month
-      start_day = match[3] || today.day
-      start_hour = match[4]
-      start_min = match[5]
-      start_sec = 0
-      @start = Time.new(start_year, start_month, start_day,
-                        start_hour, start_min, start_sec)
+      @start = Time.new(match[1] || today.year, match[2] || today.month, match[3] || today.day,
+                        match[4], match[5], 0)
     end
   end
 end
