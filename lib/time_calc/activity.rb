2c8f471ed32ec28e8725676e73118f440192915e
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-05-23 16:46:03 -0400

    Simplify parsing implementation.
    Change implementation of Activity.new() to use regular expressions to
    parse time stamp components (both optional and mandatory).

--- lib/time_calc/activity.rb
+++ lib/time_calc/activity.rb
@@ -2,35 +2,19 @@ module TimeCalc
   class Activity
     attr_reader :start
 
-    def initialize(start_text, project)
-      if start_text.size == 4
-        today = Time.new
-        start_year, start_month, start_day = today.year, today.month, today.day
-        start_hour = start_text.slice(0, 2).to_i
-        start_min = start_text.slice(2, 2).to_i
-        start_sec = 0
-        @start = Time.new(start_year, start_month, start_day,
-                          start_hour, start_min, start_sec)
-      elsif start_text.size == 8
-        today = Time.new
-        start_year = today.year
-        start_month = start_text.slice(0, 2).to_i
-        start_day = start_text.slice(2, 2).to_i
-        start_hour = start_text.slice(4, 2).to_i
-        start_min = start_text.slice(6, 2).to_i
-        start_sec = 0
-        @start = Time.new(start_year, start_month, start_day,
-                          start_hour, start_min, start_sec)
-      elsif start_text.size == 12
-        start_year = start_text.slice(0, 4).to_i
-        start_month = start_text.slice(4, 2).to_i
-        start_day = start_text.slice(6, 2).to_i
-        start_hour = start_text.slice(8, 2).to_i
-        start_min = start_text.slice(10, 2).to_i
-        start_sec = 0
-        @start = Time.new(start_year, start_month, start_day,
-                          start_hour, start_min, start_sec)
-      end
+    def initialize(start_text, _project)
+      today = Time.new
+      time_stamp_pattern = /(\d{4}(?=\d{2}\d{2}\d{2}\d{2}))?(\d{2})?(\d{2})?(\d{2})(\d{2})/
+      match = time_stamp_pattern.match(start_text)
+      raise ArgumentError, "Invalid starting time format: #{start_text}." unless match.size > 0
+      start_year = match[1] || today.year
+      start_month = match[2] || today.month
+      start_day = match[3] || today.day
+      start_hour = match[4]
+      start_min = match[5]
+      start_sec = 0
+      @start = Time.new(start_year, start_month, start_day,
+                        start_hour, start_min, start_sec)
     end
   end
 end
